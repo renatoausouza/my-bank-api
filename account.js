3c808aaaf715741f5ce036feb510ec59e464ebb2
Renato Souza <renato.souza92@gmail.com>
2020-08-08 15:58:42 -0300

    add method GET

--- account.js
+++ account.js
@@ -4,18 +4,31 @@ import { promises as fs } from 'fs';
 const router = express.Router();
 const { readFile, writeFile } = fs;
 
+global.fileName = 'accounts.json';
+
 router.post('/', async (req, res) => {
   try {
     let account = req.body;
-    const data = await readFile('accounts.json');
+    const data = await readFile(global.fileName);
     const jsonAccounts = JSON.parse(data);
 
     account = { id: jsonAccounts.nextId, ...account };
     jsonAccounts.nextId++;
     jsonAccounts.accounts.push(account);
 
-    await writeFile('accounts.json', JSON.stringify(jsonAccounts, null, 2));
+    await writeFile(global.fileName, JSON.stringify(jsonAccounts, null, 2));
     res.send(account);
+  } catch (err) {
+    res.status(400).send({ error: err.message });
+  }
+});
+
+router.get('/', async (req, res) => {
+  try {
+    const data = await readFile(global.fileName);
+    const jsonAccounts = JSON.parse(data);
+    delete jsonAccounts.nextId;
+    res.send(jsonAccounts);
   } catch (err) {
     res.status(400).send({ error: err.message });
   }
